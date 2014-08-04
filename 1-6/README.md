076a7aa0b98461f88534f3a23e9f6202b0c184a0
仔仔 <370985116@qq.com>
2014-08-04 20:45:27 +0800

    add

--- 1-6/README.md
+++ 1-6/README.md
@@ -1,1 +1,31 @@
-#文件上传
+#index.js
+注册handle的方法
+#requestHandler.js
+```javascipt
+function start(reponse){
+	// 实现上传文件的html
+}
+function uplod(response,request){
+	var form = new fomiable.IcomingForm()
+	form.parse(request,function(err,fields,files){
+		fs.renameSync(files.upload.pth,"./tmp/test.png");
+		response.writeHead(200, {"Content-Type": "text/html"});
+    	response.write("received image:<br/>");
+    	response.write("<img src='/show' />");
+	})
+}
+function show(response){
+	fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+}
+```
--- 1-6/server.js
+++ 1-6/server.js
@@ -1,29 +1,15 @@
-var http = require('http')
-var url = require('url')
+var http = require("http");
+var url = require("url");
 
-function start(route,handle) {
-	function onRequest(request, response) {
-	    var pathname = url.parse(request.url).pathname;
- 		var postData = "";
-	    request.setEncoding("utf8");
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
 
-    	request.addListener("data", function(postDataChunk) {
-      		postData += postDataChunk;
-      		console.log("Received POST data chunk '"+
-      		postDataChunk + "'.");
-    	});
-
-    	request.addListener("end", function() {
-      		route(handle, pathname, response, postData);
-    	});
-
-	    // response.writeHead(200, {"Content-Type": "text/plain"});
-   		// response.write("Hello World");
-   		// response.end();
-	}
-
-	http.createServer(onRequest).listen(8888);
-  	console.log("Server has started.");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
